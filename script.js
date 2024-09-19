7c3b9275a0eccc93fbfec0f5b7e17fb19e3b96c2
Michael Ponce <michaelalexisponce15@gmail.com>
2024-09-19 20:33:49 +0800

    :wrench: Initial JS File

--- script.js
+++ script.js
@@ -1,50 +1,128 @@
-const file = "test-json.json";
-const root = document.getElementById('content');
-const date_div = document.getElementById('date');
+// Test Files
+const latestfile = "json/newswire.json";
+const popularfile = "json/most-pop.json";
+const foodfile = "json/food.json";
+const opinionfile = "json/opinion.json";
 
-// fetch(file)
-//     .then(result => result.json())
-//     .then(data => {
-//         console.log(data.results);
-//         data.results.forEach(item => {
-//             console.log(item)
-//         });
-//     })
-getDate();
+// API Test
+// const latestfile = "https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=yourkey";
+// const popularfile = "https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=yourkey";
+// const foodfile = "https://api.nytimes.com/svc/topstories/v2/food.json?api-key=yourkey";
+// const opinionfile = "https://api.nytimes.com/svc/topstories/v2/opinion.json?api-key=yourkey";
 
-async function getDate() {
-    let today = new Date();
-    let day = today.getDay();
-    let weekdays = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
-    let month = today.getMonth();
-    let months = new Array('January', 'February', 'March', 'April', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
-    let date = today.getDate();
-    let year = today.getFullYear();
+// Containers
+const popStories = document.getElementById('popular-article');
+const latestNews = document.getElementById('latest-article');
+const foodStories = document.getElementById('food-article');
+const opinionArticle = document.getElementById('opinion-article');
 
-    let current_day;
+// Read
+getData(popularfile, popStories, 'popular');
+getData(latestfile, latestNews, 'latest');
+getData(foodfile, foodStories, 'food');
+getData(opinionfile, opinionArticle, 'opinion');
 
-    current_day = weekdays[day] + ', ' + months[month - 1] + ' ' + date + ', ' + year
+function getImage(article){
+    if (article.multimedia && article.multimedia.length > 0){
+        return article.multimedia[0].url;
+    }   
+    return null;
+}
+function loopPopular(articles, root){
+    for (let i = 0; i < 5; i++){
+        const article = articles[i];
+        const articleContainer = document.createElement('article');
+        const imageContainer = document.createElement('img');
+        const anchorContainer = document.createElement('a');
+        const authorContainer = document.createElement('p');
 
-    const h1_date = document.createElement('h1');
-    h1_date.innerText = current_day;
-    date_div.appendChild(h1_date);
-    console.log(current_day);
+        if (i === 0){
+            articleContainer.style.width = '21rem';
+            articleContainer.style.height = 'auto';
+        }
+        // Set up image
+        imageContainer.src = getImage(article);
+
+        // Set up title anchor
+        anchorContainer.href = article.url;
+        anchorContainer.innerHTML = article.title;
+
+        // Set up author
+        authorContainer.innerHTML = article.byline;
+
+        // Add the children to the container
+        // articleContainer.appendChild(imageContainer);
+        articleContainer.appendChild(anchorContainer);
+        articleContainer.appendChild(authorContainer);
+
+        // Add the container to the root
+        root.appendChild(articleContainer);
+    }
+}
+function loopLatest(articles, kind, root){
+    let loopLength;
+
+    if (kind === 'latest'){
+        loopLength = 10;
+    }else{
+        loopLength = 3;
+    }
+
+    for (let i = 0; i < loopLength; i++){
+        const article = articles[i];
+        const articleContainer = document.createElement('article');
+        const imgDiv = document.createElement('div');
+        const txtDiv = document.createElement('div');
+        const imageContainer = document.createElement('img');
+        const anchorContainer = document.createElement('a');
+        const authorContainer = document.createElement('p');
+        
+        // Set Attri
+        imgDiv.setAttribute('class', 'img-div');
+        txtDiv.setAttribute('class', 'text-div');
+
+        // Set up image
+        const imageUrl = getImage(article);
+        if (imageUrl){
+            imageContainer.src = imageUrl;
+        }else{
+            imageContainer.src = 'https://res.cloudinary.com/dzmhkee5i/image/upload/c_scale,h_75,w_75/nyt-clone/jjyajof6l8qkaprtldxx.jpg';
+        }
+
+        // Set up title anchor
+        anchorContainer.href = article.url;
+        anchorContainer.innerHTML = article.title;
+
+        // Set up author
+        authorContainer.innerHTML = article.byline;
+
+        // Append the Children/Element
+        imgDiv.appendChild(imageContainer);
+        txtDiv.appendChild(anchorContainer);
+        txtDiv.appendChild(authorContainer);
+
+        articleContainer.appendChild(imgDiv);
+        articleContainer.appendChild(txtDiv);
+
+        // Add the container to the root
+        root.appendChild(articleContainer);
+    }
 }
 
-getFile(file)
-
-async function getFile(file) {
-    let result = await fetch(file);
-    let data = await result.json();
-    let items_arr = await data.results;
-    items_arr.forEach(item => {
-        // console.log(item)
-        let title = item.title;
-        // console.log(title)
-
-        const h1 = document.createElement('h1');
-        h1.innerText = title;
-        root.appendChild(h1);
-    })
-
+function updateData(articles, root, kind){
+    if (kind === 'popular'){
+        loopPopular(articles, root);
+    }else{
+        loopLatest(articles, kind, root);
+    }
 }
+
+async function getData(file, root, kind) {
+    const result = await fetch(file);
+    const data = await result.json();
+    updateData(data.results, root, kind);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log(window.innerWidth);
+});
